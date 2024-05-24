d703dcd1f34426cbef07a72b59989f43b1cd7a9e
Abdullah İslek <islekabd@gmail.com>
2024-05-24 23:22:21 +0300

    v1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,21 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
 
-        //version 1
-        System.out.println("hello git");
-        System.out.println("git@. : staging areaya alır");// yesilse
 
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println( "hello git");
     }
 }
