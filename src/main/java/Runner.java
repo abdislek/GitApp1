ce1d6db35749d0d6397566c5cd548428810a5a01
Abdullah İslek <islekabd@gmail.com>
2024-05-24 23:27:49 +0300

    v5

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,7 +1,12 @@
 public class Runner {
     public static void main(String[] args) {
 
-
+ // versiyon 1
         System.out.println( "hello git");
+
+        //versiyon 2
+        System.out.println("v2");
+        // versiyon 3
+        System.out.println("v3");
     }
 }
