dd8aa19f8e9abd310b108304bca71ca4f15b6cc4
Abdullah İslek <islekabd@gmail.com>
2024-05-24 22:23:40 +0300

    buraya versiyonu takit için bir kısa mesaj yazılır

---
+++ src/main/java/Runner.java
@@ -0,0 +1,21 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //version 1
+        System.out.println("hello git");
+        System.out.println("git@. : staging areaya alır");// yesilse
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
